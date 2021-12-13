02b2125fa51b826315108b041734f2e36e85fe08
HHS <qazplm1021@naver.com>
2021-12-13 13:32:36 +0900

    로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     loginBtn = document.querySelector("button");
@@ -19,6 +20,15 @@ function login() {
         },
         body: JSON.stringify(req),
     }).then((res) => res.json())
-      .then(console.log);
+      .then((res) =>{
+          if(res.success) {
+              location.href = "/";
+          } else{
+              alert(res.msg);
+          }
+      })
+      .catch(err => {
+            console.error(new Error("로그인 중 에러 발생"));
+      });
     
 }
