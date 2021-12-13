13e59bcb86d8b3f5d39bc0f960f065261950c046
HHS <qazplm1021@naver.com>
2021-12-13 13:20:06 +0900

    로그인 인증 기능

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -2,15 +2,45 @@
 
 
 
-const hello = (req, res)=>{
-    res.render("home/index");
-}
+const output = {
+    hello: (req, res)=>{
+        res.render("home/index");
+    },
 
-const login = (req, res)=>{
-    res.render("home/login");
-}
+    login: (req, res)=>{
+        res.render("home/login");
+    },
+};
+
+const users = {
+    id: ["하현수" , "현수" , "HHS"],
+    psword: ["123" , "1234", "321"],
+}; 
+
+const process = {
+    login: (req, res)=>{
+        const id = req.body.id,
+        psword = req.body.psword;
+    
+        if(users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다.",
+        });
+
+    }
+};
+
 
 module.exports = {
-    hello,
-    login,
+    output,
+    process
 };
