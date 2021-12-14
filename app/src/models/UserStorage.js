7648580f895352b596bdca0eab5d5c36da50559e
HHS <qazplm1021@naver.com>
2021-12-14 17:52:41 +0900

    로그인 기능 최적화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -18,6 +18,18 @@ class UserStorage{
         return newUsers;
     }
 
+    static getUserInfo(id){
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKey = Object.keys(users);
+        const userInfo = usersKey.reduce((newUsers, info) =>{
+            newUsers[info] = users[info][idx];
+            return newUsers;
+        }, {});
+        
+        return userInfo; 
+    }
+
 }
 
 module.exports = UserStorage;   
