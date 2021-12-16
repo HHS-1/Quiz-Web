cb7710285a1a31359e4d9f30b79d7dacb22ed963
HHS <qazplm1021@naver.com>
2021-12-16 17:19:11 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -7,6 +7,7 @@ const router = express.Router( );
 
 router.get("/",ctrl.output.hello);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 module.exports = router;
